c9f8eec64ae07d012213304a6bf5d506224eff79
michal-kapala <mim4k1@gmail.com>
2024-10-21 18:31:27 +0200

    add profile logo

--- components/profile/header_templ.go
+++ components/profile/header_templ.go
@@ -217,7 +217,7 @@ func Header(user *models.User) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = Rank(user.Rank).Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = Logo().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
